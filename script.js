bb3067a5a367e42366b8049cd9f208f3ee994dca
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-08-14 23:42:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const mainContainer = document.querySelector('body');
 const switchContainer = document.querySelector('.switch-container');
 const switchBtn = document.querySelector('.switch')
+const hour = document.querySelector('.hour')
+const min = document.querySelector('.min')
+const tick =  document.querySelector('.tick')
+const dot = document.querySelector('.dot')
