0fa9e71e1bffe017d06a6ac20d29d5913740a46f
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-08-14 23:45:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const hour = document.querySelector('.hour')
 const min = document.querySelector('.min')
 const tick =  document.querySelector('.tick')
 const dot = document.querySelector('.dot')
+
+
+setInterval(() => {
+    let day =   new Date();
+    let hr = day.getHours() * 30;
+    let mins = day.getMinutes() * 6
+    let sec = day.getSeconds() * 6
